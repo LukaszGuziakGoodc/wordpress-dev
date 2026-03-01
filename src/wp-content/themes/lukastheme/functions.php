9ff600d300461492b9720ef56cf2813bb53f862e
LukaszGuziakGoodc <lukasz.guziak@goodc.pl>
2026-03-01 17:27:07 +0100

    feat: simplify asset loading and add JS support

--- src/wp-content/themes/lukastheme/functions.php
+++ src/wp-content/themes/lukastheme/functions.php
@@ -1,9 +1,22 @@
 <?php
+/**
+ * LukasTheme Functions
+ */
+
 function lukastheme_assets() {
-    // Ładujemy główny styl motywu
-    wp_enqueue_style('main-style', get_stylesheet_uri());
+    // 1. Ładujemy główny styl (style.css)
+    wp_enqueue_style('lukas-style', get_stylesheet_uri());
+
+    // 2. Ładujemy plik JS
+    wp_enqueue_script(
+        'lukas-main-js', 
+        get_template_directory_uri() . '/js/main.js', 
+        array(), 
+        '1.0', 
+        true 
+    );
 }
 add_action('wp_enqueue_scripts', 'lukastheme_assets');
 
-// Dodajemy wsparcie dla tytułów stron (żeby nie wpisywać <title> ręcznie)
+// Dodajemy wsparcie dla tytułów stron
 add_theme_support('title-tag');
---
+++ src/wp-content/themes/lukastheme/js/main.js
@@ -0,0 +1,1 @@
+console.log("LukasTheme JS działa!");
